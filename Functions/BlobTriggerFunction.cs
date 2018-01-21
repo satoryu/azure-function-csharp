7eb6ed07a35a2c9d6f37f153789b3d991bedc82d
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2018-01-21 15:15:12 +0900

    Initial Commit

---
+++ Functions/BlobTriggerFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Function
+{
+    public class BlobTriggerFunction
+    {
+        [StorageAccount("MyBlobStorage")]
+        [FunctionName("BlobTriggerFunction")]
+        public static void Run([BlobTrigger("uploaded/{name}")] Stream myBlob, string name, TraceWriter log)
+        {
+            log.Info($"BlobTrigger function processed: {name} ({myBlob.Length}");
+        }
+    }
+}
